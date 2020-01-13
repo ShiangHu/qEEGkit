1aa1fdc12dcf0ba80f0e36edb5666da0aa92b152
shianghu <shiandyhu@gmail.com>
2020-01-13 15:05:45 +0800

    update

--- eeglabstop.m
+++ eeglabstop.m
@@ -1,6 +1,8 @@
 function eeglabstop
 if ispc
-    rmpath('E:\OneDrive - Neuroinformatics Collaboratory\Scripting\Toolbox\eeglab');
-else
-    rmpath('/media/shu/hdd/toolbox/eeglab14_1_2b');
+    eeglabpath = 'E:\OneDrive - Neuroinformatics Collaboratory\Scripting\Toolbox\eeglab';
+elseif isunix
+    eeglabpath = '/media/shu/hdd/toolbox/eeglab14_1_2b';
+end
+rmpath(eeglabpath);
 end
