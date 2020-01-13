7671607c2215fd4e932186e2aca2ba0928bf058b
shianghu <shiandyhu@gmail.com>
2020-01-13 19:49:15 +0800

    1st commit

---
+++ BSTstop.m
@@ -0,0 +1,26 @@
+function BSTstop
+% quite Brainstorm
+
+pathCell = regexp(path, pathsep, 'split');
+if isunix
+    bstpath = '/media/shu/hdd/toolbox/brainstorm3';
+    onPath = any(strcmp(bstpath, pathCell));
+elseif ispc
+    bstpath = 'E:\OneDrive - Neuroinformatics Collaboratory\Scripting\Toolbox\brainstorm\brainstorm3';
+    onPath = any(strcmpi(bstpath, pathCell));
+end
+
+if onPath&&brainstorm('status')
+    
+    brainstorm stop; clc;
+    ext = genpath(fullfile(bstpath,'external'));
+    toolbox = genpath(fullfile(bstpath,'toolbox'));
+    rmpath(bstpath,ext,toolbox)
+    
+elseif onPath&&~brainstorm('status')
+    
+    ext = genpath(fullfile(bstpath,'external'));
+    toolbox = genpath(fullfile(bstpath,'toolbox'));
+    rmpath(bstpath,ext,toolbox)
+    
+end
