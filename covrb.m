66953734a7fd97ef15070ba886d4a76bc6294151
Shiang Hu <shiandyhu@gmail.com>
2020-10-31 11:13:19 +0800

    update

--- covrb.m
+++ covrb.m
@@ -1,7 +1,7 @@
-function [S_RB,S,rk,f] = covrb(data,nw,fs)
+function [S_RB,S,rk,f] = covrb(data,varargin)
 % Covariance regularization using the rao blackwell estimator to make it SPD
 % Input
-%   data --- nchannel by sl by ns
+%   data --- EEG data (nchannel by sl by ns) or cross spectrum (chn-chn-freq) 
 %     nw --- time-bandwidth product, 2, 2.5, 3, 3.5, 4
 %     fs --- sampling frequency
 % Output
@@ -9,25 +9,29 @@ function [S_RB,S,rk,f] = covrb(data,nw,fs)
 %       S    --- covariance estimated by multitaper method
 %       rk   ---  nf*2, the rank of S, S_RB
 
-
 % See paper: Walden, A T. “Random Matrix Derived Shrinkage of Spectral Precision Matrices.” 
 % IEEE TRANSACTIONS ON SIGNAL PROCESSING 63, no. 17 (2015): 11. https://doi.org/10.1109/TSP.2015.2443726.
 
 
-% multitaper cross spectrum
-[S, f] = xspt(data,nw,fs,30);
+if nargin==3
+	nw = varargin{1};
+	fs = varargin{2};
+	[S, f] = xspt(data,nw,fs,30); % multitaper cross spectrum
+elseif nargin==1&&size(data,3)>1
+	 S = data;
+	nw = 2.5;
+    f=0.3906*(1:49);
+end
+
 [nc,~,nf] = size(S);
 S_RB = zeros(nc,nc,nf);
-
 k = 2*nw-1;
 I = eye(nc);
-muv = zeros(nf,1);
 % RB estimator
 
 for i=1:nf
 Si = S(:,:,i);
 mu = trace(Si)/nc;
-muv(i)=mu;
 
 nom = trace(Si)^2-trace(Si*Si')/k;
 den = (k+1)*(trace(Si*Si')-trace(Si)^2/nc);
