38ba1db99d5ccd89221e5336c5b7ac0125ea4800
Shiang Hu <shiandy@gmail.com>
2021-08-13 14:41:52 +0800

    rewrite

--- qcspectra.m
+++ qcspectra.m
@@ -1,65 +1,89 @@
-function [pro, rkr]= qcspectra(EEG,nw,fs,fm,varargin)
+function [pro, rkr, f]= qcspectra(EEG,nw,fs,varargin)
 % Quality check of cross spectra
 % Input
 %       EEG: data (nc-nt-ns / nc-nt)
 %       nw: time-halfbandwidth product
 %       fs: sampling rate
-%       fm: maximum frequency
-%       loc --- channel position supported by readlocs
-%       svpath
+%       fmax: maximum frequency stored in varargin{1}
+%       fmin: minimum frequency  stored in varargin{2}
+%       loc --- chanlocs varargin{3}
+%       svpath --- varargin{4}
 % Output
 %         pro ---- proportion
 %         rkr ---- [rank of the EEG waveforms, the power spectra and
 %    crossspectr at 10Hz, mean correlation interchannels powers topomaps]
+%
+% The sum of power spectra equals to the sum of explained variance/engergy (eigenvalues)
+
 
 % Shiang Hu, Jan. 9, 2020
 
-idx = EEG(:,1)~=0&~isnan(EEG(:,1));
-[S,f,nss] = xspt(EEG(idx,:),nw,fs,fm);
+fmax = varargin{1};   % filtering
+fmin = varargin{2};   % filtering
+[S, f, nss] = xspt(EEG,nw,fs,fmax,fmin);
+fbd = [fmin fmax];
 
 % idxing and referencing
 [nc,~,nf] = size(S);
 n = nss*ones(1,nf);
-pmax = 1; lmax = 50;
+pmax = 10; 
+lmax = 50;
 
 % CPC
-[Lambda,Q] = CPCstepwise1(S,n,pmax,lmax);
-Sd = tdiag(S);
-pro = sum(abs(Lambda(1:pmax,:)),2)/sum(abs(Sd(:)));
+[lmd,Q] = CPCstepwise1(S,n,pmax,lmax);
+Q = Q(:,1:pmax);
+lmd = abs(lmd(1:pmax,:)');  % freq by CPs
+psd = abs(tdiag(S));        % get the multichannel psd
+ssd = sum(psd(:));          % sum of powers (explained variance)
+% ssl = sum(lmd(:));        % sum of eigenvalues
+
+% palos index
+profd = lmd(:,1)/ssd;               % frequency dependent palosi of 1st cp
+procd = sum(lmd)/ssd;               % component dependent palosi
+% proch = Q./repmat(sum(Q.^2),[nc,1]);% channal weights in the CPC
+pro = sum(profd);                   % total palosi 
 
 % check if plot results
-if nargin==6
-    loc = varargin{1};
-    svpath = varargin{2};
-    
+if nargin==7
     figure
     % eigenvalues
-    subplot(231), semilogy(real(Lambda')), xlim([min(f) max(f)]); xlabel('Freq'); title('Eigenvalues in log scale');
-    subplot(234), plot(real(Lambda')), xlim([min(f) max(f)]); xlabel('Freq');title('Eigenvalues in nat scale');
+    subplot(231), semilogy(real(lmd)), 
+    xlim(fbd); xlabel('Freq'); title('Log engergy');
+
+    subplot(234), plot(real(lmd)), 
+    xlim(fbd); xlabel('Freq'); title('Natural engergy');
     
     % proportion of components
-    subplot(232), pareto(sum(abs(Lambda),2)./sum(abs(Lambda(:)))); title('Proportion of cumulated > 95% variance');
-    t1 = cumsum(sum(abs(Lambda),2))/sum(abs(Lambda(:)));
-    t2 = (sum(abs(Lambda),2))./sum(abs(Lambda(:)));
-    subplot(235), plot(1:nc,t1,'.',1:nc,t2,'^'); legend({'CumPro','VarPro'}); xlim([1 pmax]); xlabel('Components');
-    title('Proportion of individual components');
+    subplot(232), pareto(procd); 
+    title('Proportion of cumulated > 95% variance');
+
+    subplot(235),
+    plot(1:pmax,cumsum(procd),'.',1:pmax,procd,'^'); 
+    legend({'CumPro','VarPro'}); xlim([1 pmax]); xlabel('Ordered CPC');
+    title('Proportion of each CPC');
     
     % components
-    subplot(233), imagesc(abs(Q)),xlabel('Components'), ylabel('Channels');
-    title('Pattern of components');
+    subplot(233), imagesc(real(Q)), colorbar;
+    xlabel('Ordered CPC'), ylabel('Channels'); title('CPC patterns');
     
-    subplot(236);
-    for i=1
-        %     subplot(2,4,i)
-        topoplot(abs(Q(:,i)),loc), title(num2str(i));
+    subplot(236),
+    topoplot(real(Q(:,1)),varargin{3},'style','map'); title('CPC1: map');
+    fg=gcf; fg.Position = [24 317 1105 585];
+    saveas(gcf,[varargin{4},'_par'],'svg');
+
+    figure,
+    for i=1:8
+    subplot(4,2,i)
+    topoplot(real(Q(:,i)),varargin{3},'style','map'); title(['CPC:',num2str(i)]);
     end
-    title('Pattern of 1st component');
-    saveas(gcf,svpath);
+    fg=gcf; fg.Position = [1135 206 380 771];
+    saveas(gcf,[varargin{4},'_map'],'svg');
 end
+close all;
 
 % ouput other qc measures
-nfc = 77; % cutting off at 30Hz
-rou = triu(corr(log10(Sd(:,1:nfc))),1);
-mr = sum(rou(:))./(nfc*(nfc-1)/2);
-rkr = [rank(EEG(idx,:)), rank(Sd), rank(S(:, :, 25)), mr]; 
+rou = triu(corr(log10(psd)),1);
+mr = sum(rou(:))./(nf*(nf-1)/2);
+fid10 = (abs(f-10)==min(abs(f-10)));
+rkr = [rank(EEG), rank(psd), rank(S(:, :, fid10)), mr];
 end
