ee876d3c3b065c0569f0323c45e0865f99174e20
Shiang Hu <shiandy@gmail.com>
2021-08-14 16:26:37 +0800

    simplify

--- eeglabstart.m
+++ eeglabstart.m
@@ -7,29 +7,23 @@ function eeglabstart(varargin)
 
 % Shiang Hu, Oct. 7, 2019
 
-if ispc
-    eeglabpath = 'E:\OneDrive - CCLAB\Scripting\Toolbox\eeglab';
-    sptpath = 'E:\OneDrive - CCLAB\Scripting\Toolbox\sptkit';
-    refpath = 'E:\CCLAB\BrainWorks - Ref utilities';
-else
-    eeglabpath = '/media/shu/hdd/toolbox/eeglab14_1_2b';
-    sptpath = '/media/shu/hdd/toolbox/sptkit';
-    refpath = '/home/shu/ExpanDrive/BrainWorks/BrainWorks - Documents/EEG reference/Ref utilities';
-end
+eeglabpath = 'E:\OneDrive - CCLAB\Scripting\Toolbox\eeglab';
+sptpath = 'E:\OneDrive - CCLAB\Scripting\Toolbox\sptkit';
 
 elocpath = [eeglabpath,filesep,'sample_locs'];
-addpath(eeglabpath,elocpath,sptpath,refpath);
+addpath(eeglabpath,elocpath,sptpath);
 
 % add the starting path of eeglab
 if nargin == 0
     eeglab;
 else % no gui pops up
-    addpath(genpath(fullfile(eeglabpath,filesep,'functions')));
-    addpath(fullfile(eeglabpath,filesep,'plugins',filesep,'Biosig3.3.0',filesep,'biosig','doc'));
-    addpath(fullfile(eeglabpath,filesep,'plugins',filesep,'Biosig3.3.0',filesep,'biosig','t200_FileAccess'));
-    addpath(fullfile(eeglabpath,filesep,'plugins',filesep,'Biosig3.3.0',filesep,'biosig','t250_ArtifactPreProcessingQualityControl'));
-    rmpath (genpath(fullfile(eeglabpath,filesep,'functions','octavefunc')));
+    addpath(genpath(eeglabpath));
 end
-
 clc;
 end
+
+%     addpath(genpath(fullfile(eeglabpath,filesep,'functions')));
+%     addpath(fullfile(eeglabpath,filesep,'plugins',filesep,'Biosig3.3.0',filesep,'biosig','doc'));
+%     addpath(fullfile(eeglabpath,filesep,'plugins',filesep,'Biosig3.3.0',filesep,'biosig','t200_FileAccess'));
+%     addpath(fullfile(eeglabpath,filesep,'plugins',filesep,'Biosig3.3.0',filesep,'biosig','t250_ArtifactPreProcessingQualityControl'));
+%     rmpath (genpath(fullfile(eeglabpath,filesep,'functions','octavefunc')));
--- initpalos.m
+++ initpalos.m
@@ -1,16 +1,9 @@
-function outputfig=initpalos 
+function initpalos 
 % Initializing the PALOS running environment
 
 dpalos = 'E:\OneDrive - CCLAB\Scripting\Toolbox\PaLOS';
 
 addpath(genpath(dpalos));
-
-lsm(dpalos)
-
-dtpath = 'E:\CCLAB\ash - qceeg\5 Quality Check';
-addpath(genpath(dtpath)); % load data
-
-outputfig = 'E:\CCLAB\ash - qceeg\4 HPD_atmg_rest\figure';
 
 dref = 'E:\OneDrive - CCLAB\Scripting\Toolbox\RESTref\Unified-EEG-reference-rREST\unipolar_ref';
 addpath(dref);
